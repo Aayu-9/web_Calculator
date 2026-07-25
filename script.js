2c748b9e822ffe5a7c6762dba90f8fc4f5b697b2
Aayu-9 <301275572+Aayu-9@users.noreply.github.com>
2026-07-25 21:40:17 +0530

    fix the issue of not being able to operate
    you can not operate on multiple numbers, i wrote
    the logic by storing values in array

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-
 // functions for calculation
-function add (num1, num2) {
-  return (num1+num2);
+function add(num1, num2) {
+    return (+num1 + +num2);
 };
 
-function subctract (num1, num2) {
-	return (num1-num2);
+function subctract(num1, num2) {
+    return (num1 - num2);
 };
 
-function multiply (num1, num2)
-{
-    return (num1*num2);
+function multiply(num1, num2) {
+    return (num1 * num2);
 }
 
-function divide (num1, num2)
-{
-    return (num1/num2);
+function divide(num1, num2) {
+    return (num1 / num2);
 }
 
-const power = function(base, exponent) {
-  return base**exponent;
+const power = function (base, exponent) {
+    return base ** exponent;
 };
 
-const factorial = function(num) {
-  let answer = 1;
-	for (let i=num; i>0; i--)
-  {
-    answer *=i;
-  }
-  return answer;
+const factorial = function (num) {
+    let answer = 1;
+    for (let i = num; i > 0; i--) {
+        answer *= i;
+    }
+    return answer;
 };
 
-function operator (num1, operator, num2)
-{
+function operator(num1, operator, num2) {
     switch (operator) {
         case "+":
             return add(num1, num2);
@@ -41,13 +36,13 @@ function operator (num1, operator, num2)
             return subctract(num1, num2);
             break;
         case "*":
-            return multiply(num1, mum2);
-            break;
-        case "/":
             return multiply(num1, num2);
             break;
+        case "/":
+            return divide(num1, num2);
+            break;
         case "!":
-            return factorial(num);
+            return factorial(num1);
             break;
         case "^":
             return power(num1, num2);
@@ -56,11 +51,73 @@ function operator (num1, operator, num2)
 }
 
 const display = document.querySelector("#display");
-let array = [];
 
-function listen(e)
-{
-    display.value += e.target.id;
+
+const value = []
+const opeArr = []
+
+function listen(e) {
+    button = e.target.id;
+    display.value += button;
+    let string = "";
+
+
+    if (opeArr.length==1 || button == "=")
+    {
+        if (button != "=")
+        {
+            value.unshift(button);
+        }
+        let answer = operator(value.pop(), `${opeArr.pop()}`, value.pop());
+        display.value = answer;
+        value.length = 0;
+        value.unshift(answer);
+        opeArr.length = 0;
+    }
+
+    switch (button) {
+        case "clear":
+            display.value = "";
+            value.length = 0;
+            opeArr.length = 0;
+            string = "";
+            break;
+        case "+":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        case "-":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        case "*":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        case "/":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        case "!":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        case "^":
+            opeArr.unshift(button);
+            // value.unshift(string);
+            string = "";
+            break;
+        default:
+            string += `${button}`;
+            value.unshift(button);
+            console.log(string);
+    }
+
 }
 
 const buttons = document.querySelectorAll("button");
